79a064f5131aa61bdc5f6f0a21713b4416fde527
Brayan <brayitanisaza10@gmail.com>
2020-08-31 21:04:16 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+	import Popper from 'popper.js';
+  import 'bootstrap/dist/js/bootstrap.bundle.min';
+ 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (    
+    
+ <div class="div">
+   <h1>Tabla de integrantes</h1>
+        <table  border="2" class="tabla">
+
+<tr>
+    <th>Nombre</th>
+    <th>Apellido</th>
+    <th>Correo</th>
+    <th>Celular</th>
+</tr>
+<tr>
+    <td>Brayan   </td>
+    <td>Isaza   </td>
+    <td>brayitanisaza10@gmail.com   </td>
+    <td>3024437139   </td>
+</tr>
+<tr>
+    <td>Dairon   </td>
+    <td>Quiroga   </td>
+    <td>daironquiroga21@gmail.com   </td>
+    <td>3014528307   </td>
+</tr>
+<tr>
+    <td>Geison   </td>
+    <td>Guisao   </td>
+    <td>wwgeison465@gmail.com   </td>
+    <td>3116945103   </td>
+</tr>
+<tr>
+  <td>Timoteo   </td>
+  <td>Vallejo   </td>
+  <td>vallejo.timo15@gmailcom   </td>
+  <td>3233413254   </td>
+</tr>
+</table>
+
     </div>
+  
   );
 }
 
